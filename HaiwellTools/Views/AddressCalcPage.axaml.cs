3aedec77d0d05d3d0e3d55f66ae86f84d13615e0
Alexey Dubovik <aldk50@gmail.com>
2023-12-09 13:45:17 +0300

    save

--- HaiwellTools/Views/AddressCalcPage.axaml.cs
+++ HaiwellTools/Views/AddressCalcPage.axaml.cs
@@ -29,14 +29,14 @@ namespace HaiwellTools.Views
         {
             var clipboard = TopLevel.GetTopLevel(this)?.Clipboard;
             var dataObject = new DataObject();
-            dataObject.Set(DataFormats.Text, ((AddressCalcPageViewModel)DataContext).Component1.DecModbusAdress.ToString());
+            dataObject.Set(DataFormats.Text, ((AddressCalcPageViewModel)DataContext).Component2.DecModbusAdress.ToString());
             await clipboard?.SetDataObjectAsync(dataObject);
         }
         private async void HexCopy_Tapped2(object? sender, Avalonia.Input.TappedEventArgs e)
         {
             var clipboard = TopLevel.GetTopLevel(this)?.Clipboard;
             var dataObject = new DataObject();
-            dataObject.Set(DataFormats.Text, ((AddressCalcPageViewModel)DataContext).Component1.HexModbusAddress);
+            dataObject.Set(DataFormats.Text, ((AddressCalcPageViewModel)DataContext).Component2.HexModbusAddress);
             await clipboard?.SetDataObjectAsync(dataObject);
         }
     }
